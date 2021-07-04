cdaaa8a9c82069cd17c1caab936db938b402eca0
Junior, Ezequiel <ezequiel.cmjunior@gmail.com>
2021-07-04 18:22:54 -0300

    Add new row elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import  React, { useEffect, useState } from 'react';
 import './App.css';
+import Tmdb from './Components/Tmdb';
+import MovieRow from './Components/MovieRow';
 
-function App() {
+const App = () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
   return (
-    <div className="App">
-      
+    <div className="page">
+      <section className="lists">
+      {
+       movieList.map((item, key) => (
+          <div>
+            <MovieRow />
+          </div>
+        )
+       )
+      }
+      </section>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/MovieRow.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './MovieRow.css'
+
+const MovieRow = () => {
+    return (
+        <div>
+            Lista do Filme
+        </div>
+    );
+}
+
+export default MovieRow;
